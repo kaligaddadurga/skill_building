081e59ed9d08aed5b81ce18e58cf04c4b687e0e4
kaligaddadurga <durga_kaligadda@srmap.edu.in>
2024-07-25 08:46:40 +0530

    stegnography.py

--- stegnography.py
+++ stegnography.py
@@ -1,27 +1,24 @@
-# Python program implementing Image Steganography
-# PIL module is used to extract pixels of an image and modify it
+
 
 from PIL import Image
 
-# Convert encoding data into 8-bit binary form using ASCII value of characters
 def genData(data):
-    # List of binary codes of given data
+    
     newd = []
     for i in data:
         newd.append(format(ord(i), '08b'))
     return newd
 
-# Pixels are modified according to the 8-bit binary data and finally returned
 def modPix(pix, data):
     datalist = genData(data)
     lendata = len(datalist)
     imdata = iter(pix)
     for i in range(lendata):
-        # Extracting 3 pixels at a time
+        
         pix = [value for value in imdata.__next__()[:3] +
                imdata.__next__()[:3] +
                imdata.__next__()[:3]]
-        # Pixel value should be made odd for 1 and even for 0
+        
         for j in range(0, 8):
             if (datalist[i][j] == '0' and pix[j] % 2 != 0):
                 pix[j] -= 1
@@ -30,8 +27,7 @@ def modPix(pix, data):
                     pix[j] -= 1
                 else:
                     pix[j] += 1
-        # Eighth pixel of every set tells whether to stop or read further.
-        # 0 means keep reading; 1 means the message is over.
+        
         if (i == lendata - 1):
             if (pix[-1] % 2 == 0):
                 if (pix[-1] != 0):
@@ -50,7 +46,7 @@ def encode_enc(newimg, data):
     w = newimg.size[0]
     (x, y) = (0, 0)
     for pixel in modPix(newimg.getdata(), data):
-        # Putting modified pixels in the new image
+        
         newimg.putpixel((x, y), pixel)
         if (x == w - 1):
             x = 0
@@ -58,7 +54,6 @@ def encode_enc(newimg, data):
         else:
             x += 1
 
-# Encode data into an image
 def encode():
     img = input("Enter image name (with extension): ")
     image = Image.open(img, 'r')
@@ -70,7 +65,6 @@ def encode():
     new_img_name = input("Enter the name of the new image (with extension): ")
     newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
 
-# Decode the data from the image
 def decode():
     img = input("Enter image name (with extension): ")
     image = Image.open(img, 'r')
@@ -80,7 +74,7 @@ def decode():
         pixels = [value for value in imgdata.__next__()[:3] +
                   imgdata.__next__()[:3] +
                   imgdata.__next__()[:3]]
-        # String of binary data
+        
         binstr = ''
         for i in pixels[:8]:
             if (i % 2 == 0):
@@ -91,7 +85,7 @@ def decode():
         if (pixels[-1] % 2 != 0):
             return data
 
-# Main Function
+
 def main():
     choice = int(input(":: Welcome to Steganography ::\n"
                        "1. Encode\n2. Decode\n"))
@@ -102,7 +96,7 @@ def main():
     else:
         raise Exception("Enter correct input")
 
-# Driver Code
+
 if __name__ == '__main__':
-    # Calling the main function
+    
     main()
